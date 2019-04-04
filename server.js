57513d432d0819a5c29f97998ff33403723ea72b
Elliot Young <elliotsyoung@gmail.com>
2019-04-04 14:52:39 -0700

    updated to have server and client files

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) =>
-{
+app.get('/', (req, res) => {
   console.log('Visitor to home route:');
   res.send('Home Page');
 });
 
-app.listen(8000, () =>
-{
+app.listen(8000, () => {
   console.log('listening on port 8000');
 });
---
+++ server_files/routers/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const MainRouter = express.Router();
+
+MainRouter.get('/', (req, res) => {
+
+});
+
+module.exports = MainRouter;
