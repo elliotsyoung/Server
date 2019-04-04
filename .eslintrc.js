2d241ebc81afae4dcbfa69761d6b0aa7dac4ac6a
Elliot Young <elliotsyoung@gmail.com>
2019-04-04 15:24:41 -0700

    removed linter

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "extends": "airbnb-base",
-  "rules":
-  {
-    "no-console": "off",
-    'brace-style': ['warn', 'allman'],
-    "implicit-arrow-linebreak": ['warn','below']
-  },
-};
